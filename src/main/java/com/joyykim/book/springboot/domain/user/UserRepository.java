8af637a80673c92496d001024394a5b87b6f3ffd
김주원 <kjw11077@naver.com>
2021-01-20 23:04:27 +0900

    feat: User 도메인 생성

---
+++ src/main/java/com/joyykim/book/springboot/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.joyykim.book.springboot.domain.user;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmailAnd(String email);
+}
